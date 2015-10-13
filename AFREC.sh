0a4764d44e1e6fe02d5341a056094832a8c0912d
Mat <matubat91@gmail.com>
2015-10-13 01:57:04 -0300

    Valida bien cuando viene un solo archivo

--- AFREC.sh
+++ AFREC.sh
@@ -51,77 +51,85 @@ do
 			TYPE=$(file "$FILE" | cut -d' ' -f2)
 			if [ "$TYPE" != "ASCII" ]
 				then #echo "$FILE no es texto"
-				$GRALOG "AFREC" "Archivo Rechazado, $FILE no es texto" "WAR"
+				$GRALOG "AFREC" "Archivo Rechazado, $FILE no contiene texto" "WAR"
 				$MOVER_A "$FILE" "$RECHDIR"
 				 #Mover Archivo que no es texto
 			fi
 		done
 
-		#4 Chequeo formato de los archivos 
-		for FILE in $NOVEDIR/*
-		do
-			if ! [[ $FILE =~ ^$NOVEDIR/[a-zA-Z]{3}_[0-9]{8}$ ]]; #sin .csv ahora
-				then #echo $FILE NO FORMATO #Mover Archivo que no es formato
-				$GRALOG "AFREC" "Archivo Rechazado, $FILE tiene formato incorrecto" "WAR"
-				$MOVER_A "$FILE" "$RECHDIR"
-			fi
-		done
-
-		#5 valido nombres de los archivos
-		if [ ! -f $MAEDIR/$ARCH_CENTRALES ]; 
-			then echo #"$MAEDIR/$ARCH_CENTRALES no existe"
-				$GRALOG "AFREC" "Archivo Maestro $MAEDIR/$ARCH_CENTRALES no existe" "ERR"
-		
+		if find "$NOVEDIR" -maxdepth 0 -empty | read v; 
+			then $GRALOG "AFREC" "$NOVEDIR quedo vacio, no intenta validar formato" "INFO" 
 		else
+			#4 Chequeo formato de los archivos 
 			for FILE in $NOVEDIR/*
 			do
-				NOMBRE=${FILE##*/} #me quita la ruta del archivo, queda solo el nombre
-				COD_CENTRAL=$(echo $NOMBRE|cut -d'_' -f1) #Parseo codigo de central y fecha
-				DATE=$(echo $NOMBRE|cut -d'_' -f2 |cut -d'.' -f1)
-				
-				if !(grep -q "^$COD_CENTRAL$SEPARATOR_MAEDIR" $MAEDIR/$ARCH_CENTRALES)
-				   then #echo "$NOMBRE codigo malo" 
-				   		$GRALOG "AFREC" "Archivo Rechazado, $FILE codigo incorrecto" "WAR"
-				   		$MOVER_A "$FILE" "$RECHDIR"
-				   		continue
-				   #mover Archivo que no tiene su codigo en el maestro.
+				if ! [[ $FILE =~ ^$NOVEDIR/[a-zA-Z]{3}_[0-9]{8}$ ]]; #sin .csv ahora
+					then #echo $FILE NO FORMATO #Mover Archivo que no es formato
+					$GRALOG "AFREC" "Archivo Rechazado, $FILE tiene formato incorrecto" "WAR"
+					$MOVER_A "$FILE" "$RECHDIR"
 				fi
-
-				if ! (date -d $DATE >> /dev/null 2>&1) #salida quiet
-				 	then 
-				 		$GRALOG "AFREC" "Archivo Rechazado, $FILE fecha incorrecta" "WAR"
-				 		$MOVER_A "$FILE" "$RECHDIR"
-				 		continue
-				 	#echo "$NOMBRE fecha mala"; #mover archivo por no tener fecha valida
-				 	DIFERENCIA_DIAS="-10"
-				else
-					DIFERENCIA_DIAS=$(( ($(date --date="$TODAY" +%s) - $(date --date="$DATE" +%s)  )/(60*60*24) ));
-				fi			
-
-				if [ "$DIFERENCIA_DIAS" -gt "$DIAS_LIMITE" ];
-					then
-						$GRALOG "AFREC" "Archivo Rechazado, $FILE fecha demasiado vieja" "WAR"
-						$MOVER_A "$FILE" "$RECHDIR" 
-						continue
-					#echo "$FILE SUPERA EL AÑO" #Mover archivo por superar un año
-				fi
-
-				if [ "$DIFERENCIA_DIAS" -lt 0 ];
-					then 
-						$GRALOG "AFREC" "Archivo Rechazado, $FILE fecha del futuro" "WAR"
-						$MOVER_A "$FILE" "$RECHDIR"
-						continue
-					#echo "$FILE ARCHIVO DEL FUTURO" #Mover archivo por ser del futuro.
-				fi
-
-				#Si el archivo sigue vivo aca lo manda a la carpeta aceptado.
-				if  [[ $FILE =~ ^$NOVEDIR/[a-zA-Z]{3}_[0-9]{8}$ ]];
-					then
-					$MOVER_A "$FILE" "$ACEPDIR"
-					$GRALOG "AFREC" "Archivo Aceptado $FILE" "INFO"
-				fi
-
 			done
+		fi
+
+		if find "$NOVEDIR" -maxdepth 0 -empty | read v; 
+			then $GRALOG "AFREC" "$NOVEDIR quedo vacio, no intenta validar codigo y fecha" "INFO" 
+		else
+			#5 valido nombres de los archivos
+			if [ ! -f $MAEDIR/$ARCH_CENTRALES ]; 
+				then echo #"$MAEDIR/$ARCH_CENTRALES no existe"
+					$GRALOG "AFREC" "Archivo Maestro $MAEDIR/$ARCH_CENTRALES no existe" "ERR"
+			
+			else
+				for FILE in $NOVEDIR/*
+				do
+					NOMBRE=${FILE##*/} #me quita la ruta del archivo, queda solo el nombre
+					COD_CENTRAL=$(echo $NOMBRE|cut -d'_' -f1) #Parseo codigo de central y fecha
+					DATE=$(echo $NOMBRE|cut -d'_' -f2 |cut -d'.' -f1)
+					
+					if !(grep -q "^$COD_CENTRAL$SEPARATOR_MAEDIR" $MAEDIR/$ARCH_CENTRALES)
+					   then #echo "$NOMBRE codigo malo" 
+					   		$GRALOG "AFREC" "Archivo Rechazado, $FILE codigo incorrecto" "WAR"
+					   		$MOVER_A "$FILE" "$RECHDIR"
+					   		continue
+					   #mover Archivo que no tiene su codigo en el maestro.
+					fi
+
+					if ! (date -d $DATE >> /dev/null 2>&1) #salida quiet
+					 	then 
+					 		$GRALOG "AFREC" "Archivo Rechazado, $FILE fecha incorrecta" "WAR"
+					 		$MOVER_A "$FILE" "$RECHDIR"
+					 		continue
+					 	#echo "$NOMBRE fecha mala"; #mover archivo por no tener fecha valida
+					 	DIFERENCIA_DIAS="-10"
+					else
+						DIFERENCIA_DIAS=$(( ($(date --date="$TODAY" +%s) - $(date --date="$DATE" +%s)  )/(60*60*24) ));
+					fi			
+
+					if [ "$DIFERENCIA_DIAS" -gt "$DIAS_LIMITE" ];
+						then
+							$GRALOG "AFREC" "Archivo Rechazado, $FILE fecha demasiado vieja" "WAR"
+							$MOVER_A "$FILE" "$RECHDIR" 
+							continue
+						#echo "$FILE SUPERA EL AÑO" #Mover archivo por superar un año
+					fi
+
+					if [ "$DIFERENCIA_DIAS" -lt 0 ];
+						then 
+							$GRALOG "AFREC" "Archivo Rechazado, $FILE fecha del futuro" "WAR"
+							$MOVER_A "$FILE" "$RECHDIR"
+							continue
+						#echo "$FILE ARCHIVO DEL FUTURO" #Mover archivo por ser del futuro.
+					fi
+
+					#Si el archivo sigue vivo aca lo manda a la carpeta aceptado.
+					if  [[ $FILE =~ ^$NOVEDIR/[a-zA-Z]{3}_[0-9]{8}$ ]];
+						then
+						$MOVER_A "$FILE" "$ACEPDIR"
+						$GRALOG "AFREC" "Archivo Aceptado $FILE" "INFO"
+					fi
+
+				done
+			fi
 		fi
 
 	fi #Finaliza chequeo de archivos nuevos en NOVEDIR
